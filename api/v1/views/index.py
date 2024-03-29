e794b9ea94cc1664b491490572d0470999f99e14
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-29 15:30:56 +0000

    Added the stats thingy in index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """
 Still don't know the exact thing yet
 """
-
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify, request
 
@@ -14,3 +14,21 @@ def status():
     if request.method == 'GET':
         whatever = {"status": "OK"}
         return jsonify(whatever)
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
