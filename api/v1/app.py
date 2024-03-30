869ff2abd67b1214f412de64bbb627bceeccd4f9
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-30 15:44:51 +0000

    Added some stuff

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python3
-""" Flask Application """
-from models import storage
+"""Endpoint (route) will be to return the status of your API"""
 import os
-from flask import Flask, jsonify
+from flask import Flask
+from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
-# flask server environmental setup
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
 
-# app variable instance of Flask
+# creating a Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-# Blueprint in api.v1.views
-app.register_blueprint(app_views)
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
+
 
 @app.teardown_appcontext
-def teardown(exception):
-    """After each request this method is called to close sqlalchemy"""
+def close(ctx):
     storage.close()
 
-# 404 handler
-@app.errorhandler(404)
-def not_found(err):
-    """
-    Handles not found errors
-    """
-    message = {"error": "Not found"}
 
-    return jsonify(message), 404
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = int(os.getenv("HBNB_API_PORT"))
+else:
+    port = 5000
 
 
 if __name__ == "__main__":
-    """ Run main Flask App"""
-    app.run(host = host, port = port)
+    app.run(host=host, port=port, threaded=True)
